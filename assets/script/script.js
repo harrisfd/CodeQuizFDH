f0810068d8ee103bce3933f81615e7fb8686e4ab
ldylicia <fharrisfoster@gmail.com>
2021-02-28 21:20:07 -0800

    working on script.js file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,40 +1,49 @@
 const startButton = document.getElementById('start-btn')
-const questtionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const ansswerButtonsElement = document.getElementById('answer-buttons')
-
-let shuffledQuestions, currentQuestionIndex;
-
-startButton.addEventListener('click', startGame)
+const questionContainerElement = document.getElementById('question-container')
+const questionContainerElement = document.getElementById('question')
+const answersButtonsElement = document.getElementById('answers-buttons')
+let shuffleQuestions, currentQuestionIndex
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    questtionContainerElement.classList.remove('hide')
+    questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
+function startGame() {
 
+}
 
 function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-
-    function showQuestion(question) {
-        questionElement.innerText = question.question
-    }
-}
-
-funtion selectAnswer() {
+    showQuestion(shuffled[currentQuestionIndex])
 
 }
 
+function showQuestion(question) {
+    questionContainerElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.textbutton.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answersButtonsElement.appendChild(button)
+    })
+
+}
+
+function selectAnswer() {
+
+}
 const questions = [
     {
         question: 'What is 2 + 2',
         answers: [
-            { text: '4', correct: true},
-            { text: '22', correct: false}
+            { text: '4', correct: true },
+            { text: '22', correct: false }
         ]
     }
 ]
