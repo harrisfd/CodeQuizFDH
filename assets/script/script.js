c8c55c2e6307ddd4d9468b0192dc4306a878811a
ldylicia <fharrisfoster@gmail.com>
2021-04-03 01:43:56 -0700

    updating code for quiz

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,22 +2,42 @@ const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
 const answersButtonsElement = document.getElementById('answer-buttons')
 const questionel = document.getElementById("question")
-let shuffleQuestions, currentQuestionIndex
+const timerl = document.getElementById("timerdisplay")
+let shuffleQuestions, currentQuestionIndex, timeleft
 
 function startGame() {
-    console.log('Started')
+
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
+    timeleft = 15
+
+    timer = setInterval(() => {
+        if (timeleft >= 0) {
+            timerl.textContent = timeleft
+            timeleft--
+        }
+        else {
+            timeleft = 0
+            clearInterval(timer)
+            endquiz()
+        }
+
+    }, 1000);
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    if (currentQuestionIndex < questions.length) {
+        showQuestion(shuffledQuestions[currentQuestionIndex])
+    } else {
+        endquiz()
+    }
 
 }
 
 function showQuestion(question) {
+    answersButtonsElement.innerHTML = ""
     questionel.innerText = question.question
     question.answers.forEach(answer => {
         const button = document.createElement('button')
@@ -29,11 +49,26 @@ function showQuestion(question) {
         button.addEventListener('click', selectAnswer)
         answersButtonsElement.appendChild(button)
     })
-
+    currentQuestionIndex++
 }
 
-function selectAnswer() {
-console.log("answerbuttonwasclicked")
+function selectAnswer(event) {
+    var correct = event.target.getAttribute("data-correct")
+
+    if (!correct) {
+        timeleft -= 5
+    }
+
+
+    setNextQuestion()
+    console.log(correct)
+}
+
+
+function endquiz() {
+    // timerl.setAttribute("style","display: hidden")
+    clearInterval(timer)
+    alert("game over")
 }
 const questions = [
     {
@@ -42,9 +77,28 @@ const questions = [
             { text: '4', correct: true },
             { text: '22', correct: false }
         ]
+    },
+    {
+        question: 'What is 2 + 4',
+        answers: [
+            { text: '6', correct: true },
+            { text: '30', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 5',
+        answers: [
+            { text: '7', correct: true },
+            { text: '28', correct: false }
+        ]
     }
 ]
 
 
 
 startButton.addEventListener("click", startGame)
+document.querySelector("#initials").addEventListener("submit", function (event) {
+    event.preventDefault()
+    var intials = document.querySelector("#init").value
+    console.log(intials)
+})
